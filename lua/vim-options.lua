c5227bbcf4654c5c2d9e21125cf624f4d5a4cb59
Nyvyme <mixagame228@gmail.com>
2024-01-31 18:45:32 +0000

    Added autoindent and smarttab

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,10 +2,15 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set smarttab")
+vim.cmd("set autoindent")
 vim.cmd("set cc=80")
 vim.cmd("set mouse=a")
 vim.cmd("set number")
 vim.cmd("set ttyfast")
 vim.g.mapleader = " "
 
+vim.cmd("set backspace=indent,eol,start")
+vim.keymap.set("i", "<C-BS>", "<C-W>", {})
+vim.keymap.set("i", "<C-h>", "<C-w>", {})
 
