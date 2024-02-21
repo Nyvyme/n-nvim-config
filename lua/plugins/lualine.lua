2dd5cb39d958a6aabb5f4148bc0669b72425fcdc
Nyvyme <mixagame228@gmail.com>
2024-02-21 10:19:44 -0500

    Updated theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,8 @@ return {
   'nvim-lualine/lualine.nvim',
   config = function()
     require('lualine').setup({
+      theme = "catppuccin",
       options = {
-        theme = "nordic",
         component_separators = '|',
         section_separators = { left = '', right = '' },
       },
