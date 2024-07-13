91aabd4a9241ab3f693fb674f410e922c875d514
Nyvyme <mixagame228@gmail.com>
2024-07-13 12:55:30 +0500

    Replaced fugitive with lazygit.
    Keybind for lazygit is '<leader>lg'.

--- init.lua
+++ init.lua
@@ -14,4 +14,4 @@ vim.opt.rtp:prepend(lazypath)
 require("vim-options")
 require("lazy").setup("plugins")
 
-vim.cmd.colorscheme "catppuccin-macchiato"
+vim.cmd.colorscheme "vim"
--- lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  config = function ()
-    vim.keymap.set("n", "<leader>ga", ":Git add *<CR>", {})
-    vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
-    vim.keymap.set("n", "<leader>gP", ":Git push<CR>", {})
-  end
-}
