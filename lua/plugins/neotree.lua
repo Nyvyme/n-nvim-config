be593a38db258fcd8e96b5933acf5eb6b485b5a3
Nyvyme <mixagame228@gmail.com>
2024-09-29 12:51:27 +0500

    Updated Neotree.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -8,5 +8,15 @@ return {
   },
   config = function()
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>')
+
+    require("neo-tree").setup{
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        }
+      }
+    }
   end
 }
